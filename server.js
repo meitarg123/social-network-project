3d8e5b753a9eb79759bf3f84f0269338c76e885f
Meitar <maitarg123@gmail.com>
2023-09-27 15:41:29 +0300

    add all the data we neet to registration

--- server.js
+++ server.js
@@ -1,40 +1,51 @@
-const express = require('express'); //import express library
+const express = require('express')
+const bcrypt = require('bcrypt')
+
 const app = express(); //initialize express app
 const port = 3000; //port
-const bcrypt = require('bcrypt'); //import bcrypt library
 
 app.use(express.json()) //allow express to parse json data
 const users = []  //temp tatabase
+
+app.get('/', (req, res) => {
+    console.log('Entered website!');
+})
 
 app.get('/users', (req, res) => {
     res.json(users);
 })
 
-app.post('/users',  async (req, res) => {
-    try{
+ const encryptPassword = async (password) =>{
         const salt = await bcrypt.genSalt(); //generate salt
-        const hashedPassword = await bcrypt.hash(req.body.password, salt); //hash password
-        console.log(salt);
-        console.log(hashedPassword);
-        const user = {name: req.body.name, password: hashedPassword} //create user object
-        users.push(user); //add user to users array
-        res.status(201).send(); //send response
-    }
-    catch {
-        res.status(500).send()
-    }
+        return await bcrypt.hash(password, salt); //hash password    
+}
 
+app.post('/users/signup',  async (req, res) => {
+        try{
+        const hashedPassword = await encryptPassword(req.body.password);
+        const user = {
+            userName: req.body.userName,
+            name: req.body.name,
+            lastName: req.body.lastName,
+            city: req.body.city,
+            email: req.body.email,
+            password: hashedPassword} //create user object
+        users.push(user); //add user to database
+        res.status(201).send(); //send response
+        } catch{
+            res.status(500).send();
+        }
 })
 
 app.post('/users/login',  async (req, res) => {
-    const user = users.find(user => user.name === req.body.name); //find
+    const user = users.find(user => user.userName === req.body.userName); //find
     if(user == null){
         return res.status(400).send('User not found');
     }
     try{
         if(await bcrypt.compare(req.body.password, user.password)){
-            res.send('Success');
-        }  else {            
+            res.send('logged in');
+        }  else {   
             res.send ('Not Allowed');
         }
     } catch(error) {
